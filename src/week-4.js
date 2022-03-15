23cc5061de7ad43aa950c6a52d72326505252425
block report <brayden-dunn@live.nmit.ac.nz>
2022-03-16 09:47:52 +1300

    javascript objects

--- src/week-4.js
+++ src/week-4.js
@@ -1,7 +1,10 @@
 //TODO 
 /* 
 Javascript Variable(s) and testing
-*/ 
+*/
+const defaults = require("nodemon/lib/config/defaults")
+
+ 
 
 var firstname = "Brayden"
 var classcode = 69
@@ -9,3 +12,10 @@ var classcode = 69
 console.log(firstname)
 console.log(classcode)
 
+//Javascript Object(s)
+var full_name = 
+{ 
+first_name: "Deus",
+last_name : "The Time Lord",
+course_number:503
+}
