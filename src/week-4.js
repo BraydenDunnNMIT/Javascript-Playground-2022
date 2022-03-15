c8463a8d4e4d00a264725cc1dab0188eff8a0c6d
block report <brayden-dunn@live.nmit.ac.nz>
2022-03-16 10:47:41 +1300

    A lot of javascript learnt sort of and building HTML into js

--- src/week-4.js
+++ src/week-4.js
@@ -21,4 +21,27 @@ course_number:503
 }
 console.log(full_name)
 console.log(full_name.first_name)
-console.log(full_name[last_name])
+console.log(full_name["last_name"])
+
+//Javascript Functions
+
+function addTwoNumbers(numb1, numb2) {
+return numb1+numb2
+}
+console.log(addTwoNumbers(20,400))
+//Adding numbers together within Javascript using functions
+//ANCHOR using Functions and adding sums
+function addThreeNumbers (numb1, numb2, numb3) {
+const result = numb1+numb2+numb3 
+return console.log("sum=" + result)
+}
+console.log(addThreeNumbers(1,2,3))
+
+//Javascript Loops
+for(var i=0; i<10; i++ ){ 
+ console.log(i)   
+}
+
+
+
+
