12fa09fe4f5b70fbc6e38040bb3ecf75790e4066
block report <brayden-dunn@live.nmit.ac.nz>
2022-03-16 09:11:18 +1300

    commiting last weeks work

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 console.log("Initiating shutdown protocols, Source deletion T - 5 Minutes. :)")
+console.log("Brayden the codespindler")
 //ANCHOR Testing Javascript code and learning 9/3/2022
 //TODO Practice Javascript and get use to coding 9/3/2022
 //NOTE Just testing different Anchoring functionality within VS-Code
@@ -7,5 +8,4 @@ Remember to git add. , then git commit -m "the message" , then git push origin
 (then to whatever branch you're on) when pushing new code to upload it to the Github servers. 
 
 To create the JSON package npm init -y (the -y stands for default for a default copy and the init 
-    stands for a stortened form of the word 'initialized')
-/*
+    stands for a stortened form of the word 'initialized') */
